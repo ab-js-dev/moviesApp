bf83da7cc79b447f5c7326db8023005c27e2cf04
Ab/dc <anis.bouchenafa@tradeling.com>
2022-03-14 18:41:09 +0400

    core: add react router, pages

---
+++ src/components/movies-container.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+import { MoviesHeader } from "./movies-header";
+export function MoviesContainer() {
+  return (
+    <Box>
+      <MoviesHeader />
+    </Box>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,37 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
+import { CSSReset, extendTheme, ThemeProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MoviesPage } from "./routes/movies";
+import { MyListPage } from "./routes/my-list";
 
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/my-list" element={<MyListPage />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
